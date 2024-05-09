b0c1505bc099cefddc2aa9715325a6abfdeb2e49
Victoria Moreno <vkeimoreno@gmail.com>
2024-05-08 21:53:05 -0600

    Added data to be displayed

--- PasskeysTest/View/FeedView.swift
+++ PasskeysTest/View/FeedView.swift
@@ -1,33 +1,53 @@
 import SwiftUI
 
 struct FeedView: View {
+    var transactions: [Transaction]
     @EnvironmentObject private var app: VenmoViewController
+    //@State private var friendTransactions: [Transaction]? = nil
     @Environment(\.isSearching) private var isSearching {
         willSet {
             if newValue {
-                path.append(1) // TODO: what goes here?
+                //path.append(1) // TODO: what goes here?
             }
         }
     }
     @Environment(\.dismissSearch) private var dismissSearch
-    @Binding var path: NavigationPath
+    //@Binding var path: NavigationPath
     
+    // FAKE DATA
+    var fakeDataGenerator : FakeData = FakeData()
+   // @State private var friendTransactions: [Transaction] = []
+    
+    // TODO: use initializer to refresh friend feed
+//    init(path: NavigationPath) {
+//        self.path = path
+//        Task { [weak app] in
+//            guard let app = app else { return }
+//            await app.getFriendsFeed()
+//        }
+//        if let friendTransactionFeed = app.friendTransactionFeed {
+//            self.friendTransactions = friendTransactionFeed
+//        }
+//    }
+  
     var body: some View {
-        if isSearching {
-            
-        } else {
-            ScrollView {
-                LazyVStack {
-                    ForEach(app.friendTransactionFeed ?? []) { transaction in // TODO: force
-                        NavigationLink(value: transaction) {
-                            transactionLabel(for: transaction)
+            if isSearching {
+                // Handle searching view
+                Text("Searching...")
+            } else {
+                ScrollView {
+                    LazyVStack {
+                        // TODO: change from fake data to real data
+                        ForEach(transactions ?? []) { transaction in
+                            NavigationLink(value: transaction) {
+                                transactionLabel(for: transaction)
+                            }
+                            .buttonStyle(.plain)
                         }
-                        .buttonStyle(.plain)
                     }
                 }
             }
         }
-    }
     
     private func transactionLabel(for transaction: Transaction) -> some View {
         // TODO: refactor
@@ -52,8 +72,9 @@ struct FeedView: View {
 struct FeedView_Previews: PreviewProvider {
     static var previews: some View {
         @State var path = NavigationPath()
+        var fakeDataGenerator : FakeData = FakeData()
 
-        FeedView(path: $path)
+        FeedView(transactions: fakeDataGenerator.populateFakeData())
             .environmentObject(VenmoViewController())
     }
 }
