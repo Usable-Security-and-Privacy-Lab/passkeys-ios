4071a8f7876b475ad8ce43551dd203e9cfcb777f
Victoria Moreno <vkeimoreno@gmail.com>
2024-05-08 21:51:36 -0600

    Added feed with fake data

--- PasskeysTest/View/HomeView.swift
+++ PasskeysTest/View/HomeView.swift
@@ -4,40 +4,43 @@ struct HomeView: View {
     @EnvironmentObject private var app: VenmoViewController
     @State private var searchQuery = ""
     @State private var path = NavigationPath()
+    var fakeDataGenerator = FakeData()
     
     var body: some View {
         NavigationStack(path: $path) {
-            if app.friendTransactionFeed != nil && !app.friendTransactionFeed!.isEmpty {
-                FeedView(path: $path)
+            let user = app.currentUserProfile
+            //if app.friendTransactionFeed != nil && //!app.friendTransactionFeed!.isEmpty {
+            FeedView(transactions: fakeDataGenerator.populateFakeData())
                     .environmentObject(app)
                     .navigationTitle("Feed")
                     .venmoSearchBar(searchQuery: $searchQuery)
                     .navigationDestination(for: Transaction.self) { transaction in
-                        TransactionView() // TODO: param
+                        TransactionView(userName: user!.username, transaction: transaction)
                     }
-            } else {
+            //} else {
                 Color(.clear)
                     .navigationTitle("Feed")
-            }
+            //}
         }
-        .overlay {
-//            if false { // TODO: delete
-                if app.friendTransactionFeed == nil {
-                    ProgressView()
-                        .controlSize(.large)
-                } else if app.friendTransactionFeed!.isEmpty {
-                    VStack {
-                        Text("There's nothing here... 😕")
-                            .padding()
-                        Text("Add friends to see their transactions!")
-                    }
-                    .foregroundColor(.gray)
-                }
-//            }
-        }
+//        .overlay {
+////            if false { // TODO: delete
+//                if app.friendTransactionFeed == nil {
+//                    ProgressView()
+//                        .controlSize(.large)
+//                } else if app.friendTransactionFeed!.isEmpty {
+//                    VStack {
+//                        Text("There's nothing here... 😕")
+//                            .padding()
+//                        Text("Add friends to see their transactions!")
+//                    }
+//                    .foregroundColor(.gray)
+//                }
+////            }
+//        }
         .onAppear {
             Task {
                 await app.getFriendsFeed()
+                await app.getCurrentUserInfo()
             }
         }
     }
