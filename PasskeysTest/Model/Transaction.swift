52c6828b5bcd7bfac6350b9f6a728fc7e7673c4c
Victoria Moreno <vkeimoreno@gmail.com>
2024-05-08 21:53:45 -0600

    Added default completion date

--- PasskeysTest/Model/Transaction.swift
+++ PasskeysTest/Model/Transaction.swift
@@ -4,11 +4,11 @@ struct Transaction: Identifiable, Codable, Hashable {
     var id: Int
     var actor: Profile
     var target: Profile
-    var amount: Decimal?
+    var amount: Decimal = 0
     var action: TransactionAction
     var note: String
     var dateCreated: Date
-    var dateCompleted: Date
+    var dateCompleted: Date = Calendar.current.date(from: DateComponents(year: 1969, month: 12, day: 31))!
     var audience: TransactionAudience
 }
 
